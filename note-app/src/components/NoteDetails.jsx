240fe00e0fabc100b18df78c11f31a2ecf0a96e7
Zaynab Maleki <zizimrad@gmail.com>
2025-05-20 13:07:43 +0330

    final changes

--- note-app/src/components/NoteDetails.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NoteDetails() {
-    return (
-        <div>HI</div>
-    )
-}
--- note-app/src/components/NoteInput.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NoteInput() {
-    return (
-        <div>Input</div>
-    )
-}
